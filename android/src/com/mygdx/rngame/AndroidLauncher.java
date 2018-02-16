4a9aada1232d6e6b077496594bb9953a590bff66
fidde <fiddeg@live.se>
2018-02-16 11:08:24 +0100

    Initial commit

---
+++ android/src/com/mygdx/rngame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.rngame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.rngame.RNGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RNGame(), config);
+	}
+}
